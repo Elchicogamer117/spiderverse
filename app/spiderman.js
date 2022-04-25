11ea2ce3fe5ec37e6e652518cf1ee0c3616871fb
Cristian <cris18002011@hotmail.com>
2022-04-25 14:46:23 -0500

    TDD - Segunda validacion

--- app/spiderman.js
+++ app/spiderman.js
@@ -1,10 +1,14 @@
 class spiderman{
-      constructor(name,age, actor, movies, studio){
+      constructor(name, age, actor, movies, studio){
             this.name = name
             this.age = age
             this.actor = actor
             this.movies = movies
             this.studio = studio
+      }
+
+      getInfo(){
+            return `Hey, I'm ${this.actor} from ${this.studio} studios (${this.name})`
       }
 }
 
--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -10,6 +10,21 @@ describe("Unit test for Spiderman class", () => {
             expect(andrewGarfield.age).toBe(31)
             expect(andrewGarfield.actor).toBe('Andrew Garfield')
             expect(andrewGarfield.movies).toBe(2)
-            expect(andrewGarfield.studio).toBe('Sony')
+            expect(andrewGarfield.studio).toBe("Sony")
+      });
+})
+
+describe("Unit test for Spiderman class", () => {
+      test('2) Use method getInfo()', () => {
+            //instanciamos objeto
+            const tomHolland = new spiderman('SpiderHome', 25, 'Tom Holland', 3, 'Marvel')
+
+            //Validacion
+            expect(tomHolland.name).toBe('SpiderHome')
+            expect(tomHolland.age).toBe(25)
+            expect(tomHolland.actor).toBe('Tom Holland')
+            expect(tomHolland.movies).toBe(3)
+            expect(tomHolland.studio).toBe('Marvel')
+            expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studios (SpiderHome)")
       });
 })
