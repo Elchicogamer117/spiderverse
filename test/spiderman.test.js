1b4b0a06ad78e0154c7db79e7684fd4ec3690927
Cristian <cris18002011@hotmail.com>
2022-04-25 14:01:45 -0500

    TDD - 1.Agregando funcionalidad deseada

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,13 @@
-describe("Test Suite Dummy Description", () => {
-      test('Case 1 Dummy', () => {
-            const resultOfSomething = 1 + 2
-            expect(resultOfSomething).toBe(18);
+describe("Unit test for Spiderman class", () => {
+      test('1) Create an spiderman object', () => {
+            //instanciamos objeto
+            const andrewGarfield = new Spiderman('The amazing spiderman', 31, 'Andrew Garfield', 2, 'Sony')
+
+            //Validacion
+            expect(andrewGarfield.name).toBe('The amazing spiderman')
+            expect(andrewGarfield.age).toBe(25)
+            expect(andrewGarfield.actor).toBe('Andrew Garfield')
+            expect(andrewGarfield.movies).toBe(2)
+            expect(andrewGarfield.studio).toBe('Sony')
       });
 })
